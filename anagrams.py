4076bbc25a7bf7a20f832d6ea515c3fcb865370e
Leonardo Pozzati <leon.pozzati@gmail.com>
2022-03-01 01:47:18 +0100

    Create anagrams.py
    This is a simple example which shows how to check if two words are anagrams.

---
+++ anagrams.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+
+def dict_from_str(my_str):
+    my_dict = dict()
+    for letter in my_str:
+        if my_dict.get(letter, False):
+            my_dict[letter] += 1
+        else:
+            my_dict[letter] = 1
+    return my_dict
+
+
+def anagrams(first_str, second_str):
+    if not len(first_str) == len(second_str):
+        return False
+    return dict_from_str(first_str) == dict_from_str(second_str)
+
+
+if __name__ == '__main__':
+    str_one = "bored"
+    str_two = "robed"
+    print(str_one, str_two, " are " if anagrams(str_one, str_two) else " are not ", "anagrams")
+
+    str_one = "night"
+    str_two = "thing"
+    print(str_one, str_two, " are " if anagrams(str_one, str_two) else " are not ", "anagrams")
+
+    str_one = "dream"
+    str_two = "derma"
+    print(str_one, str_two, " are " if anagrams(str_one, str_two) else " are not ", "anagrams")
+ 
+    str_one = "mother"
+    str_two = "other"
+    print(str_one, str_two, " are " if anagrams(str_one, str_two) else " are not ", "anagrams")
+
+    str_one = "ABBA"
+    str_two = "ACAB"
+    print(str_one, str_two, " are " if anagrams(str_one, str_two) else " are not ", "anagrams")
+    
